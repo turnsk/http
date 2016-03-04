c672defe4035d4e23afbbc76e6c8c4dd42f5a774
Tomas <tomas@turn.sk>
2016-03-04 13:53:23 +0100

    Release 1.2; Input/output stream support; Input/output file support; Added put/delete HTTP methods

--- src/main/java/sk/turn/http/Http.java
+++ src/main/java/sk/turn/http/Http.java
@@ -1,7 +1,13 @@
 package sk.turn.http;
 
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -17,7 +23,7 @@ import java.util.concurrent.Executors;
 /**
  * Class representing a single HTTP request. This class wraps the Java URLConnection class for easy use in most common HTTP(S) scenarios.
  */
-public class Http {
+public class Http implements Closeable {
 
 	/**
 	 * A listener interface to receive the result of the asynchronous HTTP call.
@@ -35,6 +41,9 @@ public class Http {
 		 * @see Http#getResponseHeader(String)
 		 * @see Http#getResponseData()
 		 * @see Http#getResponseString()
+		 * @see Http#getResponseStream()
+		 * @see Http#writeResponseToStream(OutputStream)
+		 * @see Http#writeResponseToFile(File)
 		 */
 		void onHttpResult(Http http);
 	}
@@ -47,11 +56,22 @@ public class Http {
 	 * Constant for HTTP post method.
 	 */
 	public static final String POST = "POST";
+	/**
+	 * Constant for HTTP put method.
+	 */
+	public static final String PUT = "PUT";
+	/**
+	 * Constant for HTTP delete method.
+	 */
+	public static final String DELETE = "DELETE";
 	private static String ENCODING = "utf-8";
 	private static ExecutorService executor;
+	private static final int BUFFER_SIZE = 8192;
 
 	/**
 	 * Same as {@link java.net.URLEncoder#encode(String, String)} using the UTF-8 encoding and no exception throwing.
+	 * @param str The string to be URL-encoded.
+	 * @return The URL-encoded string.
 	 */
 	public static String urlEncode(String str) {
 		try { return URLEncoder.encode(str, ENCODING); }
@@ -60,6 +80,8 @@ public class Http {
 
 	/**
 	 * Same as {@link java.net.URLDecoder#decode(String, String)} using the UTF-8 encoding and no exception throwing.
+	 * @param str The string to be URL-decoded.
+	 * @return The URL-decoded string.
 	 */
 	public static String urlDecode(String str) {
 		try { return URLDecoder.decode(str, ENCODING); }
@@ -73,13 +95,17 @@ public class Http {
 	private int timeoutConnect = -1;
 	private int timeoutRead = -1;
 	private byte[] requestData;
+	private InputStream inputStream;
+	private boolean inputStreamCloseWhenRead;
 	private int responseCode;
 	private String responseMessage;
 	private Map<String, List<String>> responseHeaders;
-	private byte[] responseData;
+	private HttpURLConnection connection;
 
 	/**
 	 * Creates a HTTP object to an URL using a specific method.
+	 * @param url The URL of the resource to load. Do not append GET parameters here, use rather {@link Http#addParam(String, String)} method.
+	 * @param method The HTTP method to use, you may use {@link Http#GET}, {@link Http#POST}, {@link Http#PUT} or {@link Http#DELETE} helper fields.
 	 */
 	public Http(String url, String method) {
 		this.url = url;
@@ -89,8 +115,7 @@ public class Http {
 	}
 
 	/**
-	 * Adds a HTTP request header. If a
-	 * 
+	 * Adds a HTTP request header. If a header with the key already exists, it is overwritten.
 	 * @param key The request header name.
 	 * @param value The request header value.
 	 * @return This Http object for easy call chaining.
@@ -102,7 +127,6 @@ public class Http {
 
 	/**
 	 * Adds a request GET/POST parameter.
-	 * 
 	 * @param key The name of the parameter.
 	 * @param value The value of the parameter, the value will be encoded when sending the request.
 	 * @return This Http object for easy call chaining.
@@ -113,30 +137,7 @@ public class Http {
 	}
 
 	/**
-	 * Sets the data to send as HTTP post body.
-	 * 
-	 * @param data Raw HTTP post data.
-	 * @return This Http object for easy call chaining.
-	 */
-	public Http setData(byte[] data) {
-		requestData = data;
-		return this;
-	}
-
-	/**
-	 * Sets the string to send as HTTP post body.
-	 * 
-	 * @param data HTTP post data, will be converted to bytes using UTF-8 encoding.
-	 * @return This Http object for easy call chaining.
-	 */
-	public Http setData(String data) {
-		try { return setData(data.getBytes(ENCODING)); }
-		catch (Exception e) { return this; }
-	}
-
-	/**
 	 * Sets the connection timeout for this request in milliseconds.
-	 * 
 	 * @param ms Timeout in milliseconds.
 	 * @return This Http object for easy call chaining.
 	 */
@@ -147,7 +148,6 @@ public class Http {
 
 	/**
 	 * Sets the read timeout for this request in milliseconds.
-	 * 
 	 * @param ms Timeout in milliseconds.
 	 * @return This Http object for easy call chaining.
 	 */
@@ -157,12 +157,68 @@ public class Http {
 	}
 
 	/**
+	 * Sets the data to send as HTTP post body.
+	 * @param data Raw HTTP post data.
+	 * @return This Http object for easy call chaining.
+	 */
+	public Http setData(byte[] data) {
+		requestData = data;
+		if (inputStream != null && inputStreamCloseWhenRead) {
+			try { inputStream.close(); }
+			catch (IOException e) { }
+			inputStream = null;
+		}
+		return this;
+	}
+
+	/**
+	 * Sets the string to send as HTTP post body.
+	 * @param data HTTP post data, will be converted to bytes using UTF-8 encoding.
+	 * @return This Http object for easy call chaining.
+	 */
+	public Http setData(String data) {
+		try { return setData(data.getBytes(ENCODING)); }
+		catch (Exception e) { return this; }
+	}
+
+	/**
+	 * Sets the data to send as HTTP post body as an input stream. Note, that the input stream must be
+	 * readable and only be closed after the actual request is sent, either synchronously (after calling {@link Http#send()})
+	 * or asynchronously (in {@link Listener#onHttpResult(Http)} implementation).
+	 * @param inputStream The input stream to read the data from.
+	 * @param closeWhenRead Whether to close the input stream when the data has been read, otherwise
+	 *                         it is your responsibility to close the data after {@link Http#send()} has been called
+	 *                         or in {@link Listener#onHttpResult(Http)} implementation.
+	 * @return This Http object for easy call chaining.
+	 */
+	public Http setData(InputStream inputStream, boolean closeWhenRead) {
+		this.inputStream = inputStream;
+		inputStreamCloseWhenRead = closeWhenRead;
+		requestData = null;
+		return this;
+	}
+
+	/**
+	 * Sets the data to send as HTTP post body to be read from a file. Note, that the file must exist
+	 * when the actual request is being sent, either synchronously (during calling {@link Http#send()})
+	 * or asynchronously (before {@link Http.Listener#onHttpResult(Http)} is called).
+	 * @param file A valid {@link java.io.File} reference.
+	 * @return This Http object for easy call chaining.
+	 * @throws IOException When the supplied file cannot be opened.
+	 */
+	public Http setData(File file) throws IOException {
+		return setData(new FileInputStream(file), true);
+	}
+
+	/**
 	 * Sends the HTTP request synchronously.
-	 * 
 	 * @return This Http object for easy call chaining.
 	 * @throws IOException When anything in the network communication goes wrong.
 	 */
 	public Http send() throws IOException {
+		if (connection != null || responseCode != 0) {
+			throw new IllegalStateException("The connection is in progress or has already been used, create a new instance.");
+		}
 		String params = null;
 		if (this.params.size() > 0) {
 			StringBuilder sb = new StringBuilder();
@@ -172,42 +228,45 @@ public class Http {
 			params = sb.substring(0, sb.length() - 1);
 		}
 		URL url = new URL(this.url + (method.equalsIgnoreCase(GET) && params != null ? "?" + params : ""));
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod(method);
-		conn.setInstanceFollowRedirects(false);
-		conn.setUseCaches(false);
+		connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod(method);
+		connection.setInstanceFollowRedirects(false);
+		connection.setUseCaches(false);
 		if (timeoutConnect != -1) {
-			conn.setConnectTimeout(timeoutConnect);
+			connection.setConnectTimeout(timeoutConnect);
 		}
 		if (timeoutRead != -1) {
-			conn.setReadTimeout(timeoutRead);
+			connection.setReadTimeout(timeoutRead);
 		}
 		for (String header : headers.keySet()) {
-			conn.setRequestProperty(header, headers.get(header));
+			connection.setRequestProperty(header, headers.get(header));
 		}
-		if (method.equalsIgnoreCase(POST)) {
+		if (!method.equalsIgnoreCase(GET) && !method.equalsIgnoreCase(DELETE)) {
 			if (!headers.containsKey("Content-Type")) {
-				conn.setRequestProperty("Content-Type", requestData != null ? "application/octet-stream" : "application/x-www-form-urlencoded");
+				connection.setRequestProperty("Content-Type", requestData != null ? "application/octet-stream" : "application/x-www-form-urlencoded");
 			}
-			if (requestData == null) {
-				requestData = (params != null && params.length() > 0 ? params.getBytes(ENCODING) : new byte[0]);
+			if (inputStream != null) {
+				connection.setDoOutput(true);
+				copyStream(inputStream, connection.getOutputStream());
+				connection.getOutputStream().flush();
+				if (inputStreamCloseWhenRead) {
+					try { inputStream.close(); }
+					catch (IOException e) { }
+				}
+			} else {
+				if (requestData == null) {
+					requestData = (params != null && params.length() > 0 ? params.getBytes(ENCODING) : new byte[0]);
+				}
+				connection.setRequestProperty("Content-Length", Integer.toString(requestData.length));
+				connection.setDoOutput(true);
+				connection.getOutputStream().write(requestData);
+				connection.getOutputStream().flush();
+				requestData = null;
 			}
-			conn.setRequestProperty("Content-Length", Integer.toString(requestData.length));
-			conn.setDoOutput(true);
-			conn.getOutputStream().write(requestData);
-			conn.getOutputStream().flush();
 		}
-		responseCode = conn.getResponseCode();
-		responseMessage = conn.getResponseMessage();
-		responseHeaders = conn.getHeaderFields();
-		String contentLength = getResponseHeader("Content-Length");
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream(contentLength != null ? Integer.parseInt(contentLength) : 1024);
-		byte[] b = new byte[1024];
-		for (int read; (read = conn.getInputStream().read(b)) != -1;) {
-			buffer.write(b, 0, read);
-		}
-		conn.disconnect();
-		responseData = buffer.toByteArray();
+		responseCode = connection.getResponseCode();
+		responseMessage = connection.getResponseMessage();
+		responseHeaders = connection.getHeaderFields();
 		return this;
 	}
 
@@ -217,7 +276,6 @@ public class Http {
 	 * {@link java.io.IOException}, the {@link Http#getResponseCode()} is set to -1 and {@link Http#getResponseMessage()} 
 	 * to {@link java.io.IOException#toString()}. The listener is called from the background thread, not from the thread 
 	 * this method is called.
-	 * 
 	 * @param listener Callback interface to receive the {@link Listener#onHttpResult(Http)} notification.
 	 * @return This Http object for easy call chaining.
 	 */
@@ -242,21 +300,19 @@ public class Http {
 
 	/**
 	 * Returns the HTTP response code.
-	 * 
 	 * @return HTTP response code, e.g. 200 for success.
 	 */
 	public int getResponseCode() { return responseCode; }
 
 	/**
 	 * Returns the HTTP response status message.
-	 * 
 	 * @return HTTP response status message.
 	 */
 	public String getResponseMessage() { return responseMessage; }
 
 	/**
 	 * Returns the first HTTP response header with the specified key or null if the header does not exist.
-	 * 
+	 * @param key The header name to retrieve the first header value for.
 	 * @return First HTTP response header value or null if none such header exists.
 	 */
 	public String getResponseHeader(String key) {
@@ -265,7 +321,7 @@ public class Http {
 
 	/**
 	 * Returns a list of HTTP response headers with the specified key.
-	 * 
+	 * @param key The header name to retrieve the header values for.
 	 * @return List of HTTP response header values.
 	 */
 	public List<String> getResponseHeaders(String key) {
@@ -274,16 +330,103 @@ public class Http {
 
 	/**
 	 * Returns the raw response data.
-	 * 
 	 * @return Raw response data.
+	 * @throws IOException When the underlying response stream cannot be read.
 	 */
-	public byte[] getResponseData() { return responseData; }
+	public byte[] getResponseData() throws IOException {
+		String contentLength = getResponseHeader("Content-Length");
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream(contentLength != null ? Integer.parseInt(contentLength) : BUFFER_SIZE);
+		try {
+			copyStream(getResponseStream(), buffer);
+			return buffer.toByteArray();
+		} finally {
+			close();
+			buffer.close();
+		}
+	}
 
 	/**
 	 * Returns the response data as a string.
-	 * 
 	 * @return Response data decoded to string using UTF-8 encoding.
 	 */
-	public String getResponseString() { try { return new String(responseData, ENCODING); } catch (Exception e) { return null; } }
+	public String getResponseString() {
+		try {
+			return new String(getResponseData(), ENCODING);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/**
+	 * Returns the response stream. Note that you are responsible for closing the stream and calling
+	 * {@link #close()} when finished working with the stream.
+	 * @return The response stream.
+	 * @throws IOException When the underlying response stream cannot be opened.
+	 */
+	public InputStream getResponseStream() throws IOException {
+		if (connection == null) {
+			throw new IllegalStateException("Method send() has not been called yet or the connection was already closed.");
+		}
+		return connection.getInputStream();
+	}
+
+	/**
+	 * Writes the response data directly to a stream. The underlying connection is automatically closed
+	 * when the data is copied.
+	 * @param stream The stream to write the response data to.
+	 * @return The number of bytes written.
+	 * @throws IOException When the underlying response stream cannot be opened or read,
+	 * or when the data cannot be written to the output stream.
+	 */
+	public int writeResponseToStream(OutputStream stream) throws IOException {
+		try {
+			return copyStream(getResponseStream(), stream);
+		} finally {
+			close();
+		}
+	}
+
+	/**
+	 * Writes the response data directly to a file. The underlying connection is automatically closed
+	 * when the data is copied.
+	 * @param file A valid file to write the response data to.
+	 * @return The number of bytes written.
+	 * @throws IOException When the underlying response stream cannot be opened or read,
+	 * or when the data cannot be written to the file.
+	 */
+	public int writeResponseToFile(File file) throws IOException {
+		FileOutputStream fileStream = null;
+		try {
+			fileStream = new FileOutputStream(file);
+			return writeResponseToStream(new FileOutputStream(file));
+		} finally {
+			if (fileStream != null) {
+				fileStream.close();
+			}
+		}
+	}
+
+	/**
+	 * Closes the underlying connection. This method is automatically called from {@link Http#getResponseData()},
+	 * {@link Http#getResponseString()}, {@link #writeResponseToStream(OutputStream)} and {@link #writeResponseToFile(File)}.
+	 * You must called this method manually if you're using {@link #getResponseStream()}.
+	 */
+	@Override
+	public void close() {
+		if (connection != null) {
+			connection.disconnect();
+			connection = null;
+		}
+	}
+
+	private int copyStream(InputStream input, OutputStream output) throws IOException {
+		byte[] buffer = new byte[BUFFER_SIZE];
+		int read, total = 0;
+		while ((read = input.read(buffer, 0, buffer.length)) >= 0) {
+			output.write(buffer, 0, read);
+			total += read;
+		}
+		return total;
+	}
 
 }
